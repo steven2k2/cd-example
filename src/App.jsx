bc8e55ba6a460fc3fb7066dbd3cc173c9be6632b
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-08 11:38:17 +1100

    Fix: Test 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
     return (
         <div>
-            <h1>Continuous Deployment Example mk2</h1>
+            <h1>Continuous Deployment Example mk 3</h1>
             <p>Deployment via GitHub Actions!</p>
         </div>
     );
