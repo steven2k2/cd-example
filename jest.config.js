e63ffcfaa794e7a2e2ce5025f0e57c58e670ba16
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-08 17:31:41 +1100

    feature) Coverage reports.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 export default {
-    verbose: true,
     testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['./jest.setup.js'], // Optional setup file
+    setupFilesAfterEnv: ['./jest.setup.js'],
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Maps CSS files
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest', // Use Babel Jest for transforming JS/JSX
-    },
+    collectCoverage: true, // Enable coverage collection
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx}', // Specify files to collect coverage from
+        '!src/**/*.test.{js,jsx}', // Exclude test files
+        '!src/**/index.{js,jsx}', // Exclude index files
+        '!src/serviceWorker.{js,jsx}', // Exclude service workers
+    ],
+    coverageDirectory: 'coverage', // Output directory for coverage reports
+    coverageReporters: ['text', 'lcov'], // Report formats
 };
